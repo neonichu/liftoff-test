dbe4df0a57950d05ecef9b94aba8e8c94fd707dd
Boris Bügling <boris@icculus.org>
2013-01-31 00:54:03 +0100

    Initial Commit

---
+++ Test/BBUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BBUAppDelegate.h
+//  Test
+//
+//  Created by Boris Bügling on 31.01.13.
+//  Copyright (c) 2013 Boris Bügling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
