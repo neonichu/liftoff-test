a021ea00bd9a6f25866578dee7db250bb4524d49
Boris Bügling <boris@icculus.org>
2013-01-31 22:42:52 +0100

    Updated reset.sh

--- reset.sh
+++ reset.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
 rm -f .gitattributes .gitignore
-rm -rf Resources
-
 git reset HEAD .gitattributes >/dev/null
 git reset HEAD .gitignore >/dev/null
+
+rm -rf Resources
 git reset HEAD Resources/Default-568h@2x.png >/dev/null
 git reset HEAD Resources/Default.png >/dev/null
 git reset HEAD Resources/Default@2x.png >/dev/null
-git reset HEAD Test/en.lproj/InfoPlist.strings >/dev/null
-git reset HEAD Test.xcodeproj/project.xcworkspace/contents.xcworkspacedata >/dev/null
-git reset HEAD Test.xcodeproj/project.pbxproj >/dev/null
 
-git checkout -- Test.xcodeproj/project.pbxproj
-git checkout -- Test/en.lproj/InfoPlist.strings
+git reset HEAD Test/Default-568h@2x.png >/dev/null
+git reset HEAD Test/Default.png >/dev/null
+git reset HEAD Test/Default@2x.png >/dev/null
 git checkout -- Test/Default-568h@2x.png
 git checkout -- Test/Default.png
 git checkout -- Test/Default@2x.png
+
+git reset HEAD Test.xcodeproj/project.xcworkspace/contents.xcworkspacedata >/dev/null
+git reset HEAD Test.xcodeproj/project.pbxproj >/dev/null
+git checkout -- Test.xcodeproj/project.pbxproj
+
+git reset HEAD Test/en.lproj/InfoPlist.strings >/dev/null
+git checkout -- Test/en.lproj/InfoPlist.strings
