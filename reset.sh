e00a9381f14b09dd42f2605a901bf2a10d4513cf
Boris Bügling <boris@icculus.org>
2013-01-31 21:59:58 +0100

    Updated reset.sh

--- reset.sh
+++ reset.sh
@@ -6,6 +6,7 @@ git reset HEAD .gitattributes >/dev/null
 git reset HEAD .gitignore >/dev/null
 git reset HEAD Test/en.lproj/InfoPlist.strings >/dev/null
 git reset HEAD Test.xcodeproj/project.xcworkspace/contents.xcworkspacedata >/dev/null
+git reset HEAD Test.xcodeproj/project.pbxproj >/dev/null
 
 git checkout -- Test.xcodeproj/project.pbxproj
 git checkout -- Test/en.lproj/InfoPlist.strings
